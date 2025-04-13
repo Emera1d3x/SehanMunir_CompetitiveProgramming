c77bddf71a26e4e8d400835233f0e91b042b31a9
Sehan Munir <sehanmunirgoldar@gmail.com>
2025-04-13 15:21:02 -0400

    Create ioi13p1.cpp

---
+++ IOI_Problems/ioi13p1.cpp
@@ -0,0 +1,78 @@
+// https://dmoj.ca/problem/ioi13p1
+#include <unordered_map>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int travelTime(int N, int M, int L, int A[], int B[], int T[]){
+  int n = N, m = M, l = L; 
+  bool marked[n] = {false};
+  unordered_map<int,unordered_map<int,int>> adjListW;
+  unordered_map<int, vector<int>> adjList;
+  for (int i = 0; i < m; i++){
+    int u = A[i], v = B[i], w = T[i];
+    adjListW[u][v] = w;
+    adjListW[v][u] = w;
+    adjList[u].push_back(v);
+    adjList[v].push_back(u);
+  }
+  vector<int> chain;
+  int maxDiameter = 0;
+  for (int i = 0; i < n; i++){
+    if (marked[i]){continue;}
+    unordered_map<int, int> dist1, dist2, p;
+    stack<int> s;
+    s.push(i);
+    dist1[i] = 0;
+    marked[i] = true;
+    int farNode1 = i;
+    while (!s.empty()) {
+      int u = s.top(); s.pop();
+      for (int v : adjList[u]) {
+        if (dist1.find(v) == dist1.end()) {
+          dist1[v] = dist1[u] + adjListW[u][v];
+          s.push(v);
+          marked[v] = true;
+          if (dist1[v] > dist1[farNode1]) farNode1 = v;
+        }
+      }
+    }
+    s.push(farNode1);
+    dist2[farNode1] = 0;
+    int farNode2 = farNode1;
+    while (!s.empty()) {
+      int u = s.top(); s.pop();
+      for (int v : adjList[u]) {
+        if (dist2.find(v) == dist2.end()) {
+          dist2[v] = dist2[u] + adjListW[u][v];
+          p[v] = u;
+          s.push(v);
+          if (dist2[v] > dist2[farNode2]) farNode2 = v;
+        }
+      }
+    }
+    int diameter = dist2[farNode2];
+    maxDiameter = max(maxDiameter, diameter);
+    int curChain = diameter;
+    int u = farNode2;
+    int d = 0;
+    while (p.count(u)) {
+      int v = p[u];
+      d += adjListW[u][v];
+      int curDif = max(d, diameter - d);
+      if (curDif < curChain) {
+        curChain = curDif;
+      }
+      u = v;
+    }
+    chain.push_back(curChain);
+  }
+  sort(chain.begin(), chain.end(), greater<int>());
+  if (chain.size() == 1){
+    return maxDiameter;
+  } else if (chain.size() == 2){
+    return max(maxDiameter, chain[0] + chain[1] + l);
+  } else {
+    return max(maxDiameter, max(chain[0] + chain[1] + l, chain[1] + chain[2] + 2*l));
+  }
+}
