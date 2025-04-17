f12da533273707f4d828c7c8d375b0c2c3ab77f6
Sehan Munir <sehanmunirgoldar@gmail.com>
2025-04-17 00:38:21 -0400

    Update CompetitiveTemplate.cpp

--- CompetitiveTemplate.cpp
+++ CompetitiveTemplate.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+typedef long wong; // mandatory good luck charm
+typedef long long wongwong; // mandatory good luck charm 2
 
 int main() {
   cin.sync_with_stdio(0);cin.tie(0);
